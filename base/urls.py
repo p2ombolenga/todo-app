e20f242e7f79dd998416a5c58eb4e5963e16c579
p2ombolenga <nsabimanapeter2000@gmail.com>
2024-04-25 19:25:06 +0200

    Basic User Authentication

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from django.contrib.auth.views import LogoutView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, CustomLogoutView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
--- base/views.py
+++ base/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
+from django.views.generic.base import RedirectView
 from .models import Task
+
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+class CustomLogoutView(RedirectView):
+    url = reverse_lazy('login')
+    
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return super().get(request, *args, **kwargs)
 
 class TaskList(ListView):
     model = Task
